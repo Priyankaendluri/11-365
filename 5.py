307fa9f6103d571a0138ed81f77610777a676d22
Priyankaendluri <endlurupriyanka@gmail.com>
2025-01-11 20:07:00 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,20 @@
+print(int(input("Enter your Marks")))
+if marks<40:
+  print("fail")
+else if m>40 & m<49:
+       print("Grade E")
+else if m>40 & m<49:
+      print("Grade D")
+else if m>40 & m<49:
+      print("Grade C")
+else if m>40 & m<49:
+      print("Grade B")
+else if m>40 & m<49:
+      print("Grade A")
+else if m>40 & m<49:
+      print("Grade Ex")
+else:
+   print("Error")
+     
+      
+
